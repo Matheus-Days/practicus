7af97111c07494672208bd833bc00d446c67a727
Matheus Braga Dias <matheusbragaprofissional@gmail.com>
2024-09-08 19:17:59 -0300

    feat: create mobile version for header nav

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,7 @@
 import { createClient } from '@/prismicio';
 import { PrismicNextImage, PrismicNextLink } from '@prismicio/next';
 import Link from 'next/link';
+import MobileNav from './MobileNav';
 
 export default async function Header() {
   const client = createClient();
@@ -8,10 +9,12 @@ export default async function Header() {
   const page = await client.getSingle('configuracoes');
 
   return (
-    <header className="hidden md:flex justify-between items-center py-2 px-6">
+    <header className="flex justify-between items-center py-2 px-6 relative border-b border-accent md:border-0">
       <Link href="/"><PrismicNextImage field={page.data.logo} /></Link>
       
-      <nav className="font-display">
+      <MobileNav navegacao={page.data.navegacao} />
+
+      <nav className="font-display hidden md:block">
         <ul className="flex gap-6">
           {page.data.navegacao.map(({ link, rotulo }) => (
             <li key={rotulo}>
@@ -20,6 +23,7 @@ export default async function Header() {
           ))}
         </ul>
       </nav>
+
     </header>
   );
 }
---
+++ src/app/components/MobileNav.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { PrismicNextLink } from '@prismicio/next';
+import XIcon from '../icons/x';
+import { Fragment, useEffect, useState } from 'react';
+import { GroupField } from '@prismicio/client';
+import {
+  ConfiguracoesDocumentDataNavegacaoItem,
+  Simplify
+} from '../../../prismicio-types';
+import MenuIcon from '../icons/menu';
+
+type MobileNavProps = {
+  navegacao: GroupField<Simplify<ConfiguracoesDocumentDataNavegacaoItem>>;
+};
+
+export default function MobileNav({ navegacao }: MobileNavProps): JSX.Element {
+  const [opened, setOpened] = useState(false);
+
+  useEffect(() => {
+    if (opened) document.body.style.overflow = 'hidden';
+    else document.body.style.overflow = 'unset';
+  }, [opened]);
+
+  return (
+    <Fragment>
+      {opened && (
+        <nav className="font-display md:hidden absolute top-full left-0 z-50 w-full border-t border-accent">
+          <ul className="flex flex-col">
+            {navegacao.map(({ link, rotulo }) => (
+              <PrismicNextLink
+                field={link}
+                key={rotulo}
+                className="py-3 pr-6 border-b border-accent text-right"
+              >
+                <li>{rotulo}</li>
+              </PrismicNextLink>
+            ))}
+          </ul>
+        </nav>
+      )}
+
+      <button className="p-3 md:hidden" onClick={() => setOpened(!opened)}>
+        {opened ? <XIcon /> : <MenuIcon />}
+      </button>
+
+      {opened && (
+        <div className="absolute bg-[#F1F1F1] top-full left-0 z-40 w-screen h-screen"></div>
+      )}
+    </Fragment>
+  );
+}
