2d05e0d5da2c10563f62d3f3c1384e78493d81c5
Matheus Braga Dias <matheusbragaprofissional@gmail.com>
2024-09-11 00:22:54 -0300

    doc: remove comment

--- src/app/components/PageField.tsx
+++ src/app/components/PageField.tsx
@@ -34,7 +34,6 @@ export default async function PageField({
 
   return (
     <p className="flex items-center py-1 gap-2 border-b border-primary">
-      {/* <span className="p-8"></span> */}
       <Icon className="h-6 w-6 m-2" />
       <span className="flex gap-1 font-display font-medium text-[0.8125rem] leading-[1.2188rem] tracking-[0.0094rem] md:text-lg text-primary">
         {children}
