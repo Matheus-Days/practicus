19a564f1ed94445d2d35fd5f3db76c1b47c8dd5e
Matheus Braga Dias <matheusbragaprofissional@gmail.com>
2024-09-11 21:11:54 -0300

    fix: remove '+1' from date formating

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -3,7 +3,7 @@ import { DateField } from '@prismicio/client';
 export function formatDate(date: DateField): string {
   if (!date) return '';
   const jsDate = new Date(date.toString());
-  const day = jsDate.getDate() + 1;
+  const day = jsDate.getDate();
   const month = (jsDate.getMonth() + 1).toString().padStart(2, '0');
   const year = jsDate.getFullYear();
   return `${day}/${month}/${year}`
