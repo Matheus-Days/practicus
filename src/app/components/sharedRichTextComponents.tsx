80a4709ceefd2292ed31bf04a718cde3a6d0280c
Matheus Braga Dias <matheusbragaprofissional@gmail.com>
2024-09-26 23:38:51 -0300

    refactor: expose rich text's paragraph component

--- src/app/components/sharedRichTextComponents.tsx
+++ src/app/components/sharedRichTextComponents.tsx
@@ -1,17 +1,34 @@
 import { JSXMapSerializer } from '@prismicio/react';
+import { BaseHTMLAttributes, DetailedHTMLProps, ReactNode } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 export const commonComponents: JSXMapSerializer = {
   em: ({ children }) => <em className="italic">{children}</em>,
   strong: ({ children }) => <b className="font-medium">{children}</b>
 };
 
+type SharedParagraphProps = DetailedHTMLProps<
+  BaseHTMLAttributes<HTMLParagraphElement>,
+  HTMLParagraphElement
+>;
+
+export const SharedParagraph = ({
+  children,
+  className
+}: SharedParagraphProps) => (
+  <p
+    className={twMerge(
+      'font-body text-base md:text-lg text-primary min-h-[14px] md:min-h-4',
+      className
+    )}
+  >
+    {children}
+  </p>
+);
+
 export const richTextComponents: JSXMapSerializer = {
   ...commonComponents,
-  paragraph: ({ children }) => (
-    <p className="font-body text-base md:text-lg text-primary min-h-[14px] md:min-h-4">
-      {children}
-    </p>
-  ),
+  paragraph: ({ children }) => <SharedParagraph>{children}</SharedParagraph>,
   heading3: ({ children }) => (
     <h3 className="font-body text-[1.25rem] md:text-[1.375rem] text-primary">
       {children}
@@ -23,7 +40,9 @@ export const richTextComponents: JSXMapSerializer = {
     </h4>
   ),
   list: ({ children }) => <ul className="mt-2 pl-4 list-disc">{children}</ul>,
-  oList: ({ children }) => <ul className="mt-2 pl-4 list-decimal">{children}</ul>,
+  oList: ({ children }) => (
+    <ul className="mt-2 pl-4 list-decimal">{children}</ul>
+  ),
   listItem: ({ children }) => (
     <li className="font-body text-base md:text-lg text-primary">{children}</li>
   ),
