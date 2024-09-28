fe6c62379600deb30ca255f69eca7d564f281cf7
Matheus Braga Dias <matheusbragaprofissional@gmail.com>
2024-09-28 14:33:20 -0300

    feat: create 'Nossos clientes' page

--- src/app/nossos_clientes/page.tsx
+++ src/app/nossos_clientes/page.tsx
@@ -1,14 +1,31 @@
 import { Metadata } from 'next';
-import { SliceZone } from '@prismicio/react';
+import { PrismicRichText, SliceZone } from '@prismicio/react';
 
 import { createClient } from '@/prismicio';
 import { components } from '@/slices';
+import BoundedMain from '../components/BoundedMain';
+import HeadingBadge from '../components/HeadingBadge';
+import { subtitleComponent } from '../components/sharedRichTextComponents';
+import { PrismicNextImage } from '@prismicio/next';
 
 export default async function Page() {
   const client = createClient();
   const page = await client.getSingle('nossos_clientes');
 
-  return <SliceZone slices={page.data.slices} components={components} />;
+  return (
+    <BoundedMain>
+      <HeadingBadge as="h1" className="mb-3">{page.data.titulo}</HeadingBadge>
+      <div className="mb-3">
+        <PrismicRichText field={page.data.descricao_da_pagina} components={subtitleComponent} />
+      </div>
+      <div className="flex justify-center">
+        <PrismicNextImage field={page.data.imagem_ilustrativa} className="mb-6" />
+      </div>
+      <div className="flex flex-wrap gap-x-4 md:gap-x-6 gap-y-6 justify-center">
+        <SliceZone slices={page.data.slices} components={components} />;
+      </div>
+    </BoundedMain>
+  );
 }
 
 export async function generateMetadata(): Promise<Metadata> {
--- src/slices/CardDeCliente/index.tsx
+++ src/slices/CardDeCliente/index.tsx
@@ -1,5 +1,6 @@
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { Content, ImageFieldImage } from '@prismicio/client';
+import { PrismicNextImage } from '@prismicio/next';
+import { SliceComponentProps } from '@prismicio/react';
 
 /**
  * Props for `CardDeCliente`.
@@ -11,13 +12,26 @@ export type CardDeClienteProps = SliceComponentProps<Content.CardDeClientSlice>;
  */
 const CardDeCliente = ({ slice }: CardDeClienteProps): JSX.Element => {
   return (
-    <section
+    <div
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="inline-flex flex-col items-center gap-1 md:gap-3 min-h-[8.75rem] w-[9.75rem] md:w-[16.3125rem]"
     >
-      Placeholder component for card_de_cliente (variation: {slice.variation})
-      Slices
-    </section>
+      <div className="w-fit border border-accent shadow-md rounded-xl overflow-hidden">
+        <PrismicNextImage
+          field={slice.primary.logo_do_cliente['Tela estreita']}
+          className="md:hidden"
+        />
+        <PrismicNextImage
+          field={slice.primary.logo_do_cliente['Tela larga']}
+          className="hidden md:block"
+        />
+      </div>
+
+      <p className="font-body font-medium text-base md:text-lg text-center">
+        {slice.primary.nome_do_cliente}
+      </p>
+    </div>
   );
 };
 
