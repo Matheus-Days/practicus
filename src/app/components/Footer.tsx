c452b104d2189f7571ae1c083f0f4f82f801515d
Matheus Braga Dias <matheusbragaprofissional@gmail.com>
2024-10-27 09:41:41 -0300

    feat: add credits for design and development

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,3 +1,17 @@
+import { ReactNode } from 'react';
+
+type ExtLinkProps = {
+  href: string;
+  children: ReactNode;
+};
+
+function ExtLink({ children, href }: ExtLinkProps) {
+  return (
+    <a href={href} target='_blank' className="font-medium cursor-pointer">
+      {children}
+    </a>
+  );
+}
 export default function Footer() {
   return (
     <footer className="font-display mt-6 border-t border-accent py-[0.375rem] md:py-3 px-[1.375rem] text-primary text-center">
@@ -5,7 +19,14 @@ export default function Footer() {
         © Todos os direitos reservados.
       </p>
       <p className="text-[0.5625rem] md:text-[0.8125rem] leading-[0.6875rem] md:leading-[1.1875rem]">
-        Design por: Carina Torquato | Desenvolvido por: Matheus Braga
+        Design por:{' '}
+        <ExtLink href="https://www.linkedin.com/in/carina-torquato-174467229/">
+          Carina Torquato
+        </ExtLink>{' '}
+        | Desenvolvido por:{' '}
+        <ExtLink href="https://www.linkedin.com/in/matheus-braga-dias-6129a31b0/">
+          Matheus Braga
+        </ExtLink>
       </p>
     </footer>
   );
