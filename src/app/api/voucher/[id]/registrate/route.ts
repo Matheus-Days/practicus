d5f0483bcb71a9d945754fe5571a083c4076da52
Matheus Braga Dias <matheusbragaprofissional@gmail.com>
2025-10-12 11:11:14 -0300

    feat: change rule for voucher's registration status setting considering commitment payment

--- src/app/api/voucher/[id]/registrate/route.ts
+++ src/app/api/voucher/[id]/registrate/route.ts
@@ -2,14 +2,21 @@
 
 import { NextRequest, NextResponse } from "next/server";
 import { firestore } from "@/lib/firebase-admin";
-import { CreateVoucherCheckoutRequest, CreateVoucherCheckoutResponse, VoucherDocument } from "../../voucher.types";
+import {
+  CreateVoucherCheckoutRequest,
+  CreateVoucherCheckoutResponse,
+  VoucherDocument,
+} from "../../voucher.types";
 import { validateVoucher } from "../../utils";
 import { createErrorResponse, createSuccessResponse } from "../../../utils";
 import { CheckoutDocument } from "../../../checkouts/checkout.types";
 import { DecodedIdToken } from "firebase-admin/auth";
 import { validateAuth } from "../../../../../lib/auth-utils";
-import { createCheckoutDocumentId } from "../../../checkouts/utils";
-import { RegistrationDocument, RegistrationStatus } from "../../../registrations/registration.types";
+import { createCheckoutDocumentId, isPaymentByCommitment } from "../../../checkouts/utils";
+import {
+  RegistrationDocument,
+  RegistrationStatus,
+} from "../../../registrations/registration.types";
 
 export async function POST(
   request: NextRequest,
@@ -26,13 +33,16 @@ export async function POST(
   }
 
   const { id } = params;
-  const body = await request.json() as CreateVoucherCheckoutRequest;
+  const body = (await request.json()) as CreateVoucherCheckoutRequest;
 
   const voucherDoc = await firestore.collection("vouchers").doc(id).get();
   const voucherData = voucherDoc.data() as VoucherDocument;
 
   if (!voucherDoc.exists) {
-    return NextResponse.json({ valid: false, message: "Voucher não encontrado." }, { status: 404 });
+    return NextResponse.json(
+      { valid: false, message: "Voucher não encontrado." },
+      { status: 404 }
+    );
   }
 
   const validateVoucherResult = await validateVoucher(firestore, {
@@ -44,7 +54,10 @@ export async function POST(
     return createErrorResponse(validateVoucherResult.message, 403);
   }
 
-  const checkoutDocId: string = createCheckoutDocumentId(body.eventId, authenticatedUser.uid);
+  const checkoutDocId: string = createCheckoutDocumentId(
+    body.eventId,
+    authenticatedUser.uid
+  );
   let checkout: CheckoutDocument;
   let registration: RegistrationDocument;
 
@@ -53,15 +66,18 @@ export async function POST(
       checkoutType: "voucher",
       createdAt: new Date(),
       eventId: body.eventId,
-      status: 'completed',
+      status: "completed",
       userId: authenticatedUser.uid,
-    }
+    };
 
     const checkoutDocRef = firestore.collection("checkouts").doc(checkoutDocId);
-    
+
     const checkoutDoc = await checkoutDocRef.get();
     if (checkoutDoc.exists && checkoutDoc.data()?.status !== "deleted") {
-      return createErrorResponse("Uma outra inscrição (checkout) já existe para esse email.", 400);
+      return createErrorResponse(
+        "Uma outra inscrição (checkout) já existe para esse email.",
+        400
+      );
     }
 
     await checkoutDocRef.set(checkout);
@@ -70,7 +86,10 @@ export async function POST(
   }
 
   try {
-    const buyerCheckoutDoc = await firestore.collection("checkouts").doc(voucherData.checkoutId).get();
+    const buyerCheckoutDoc = await firestore
+      .collection("checkouts")
+      .doc(voucherData.checkoutId)
+      .get();
     const buyerCheckoutData = buyerCheckoutDoc.data() as CheckoutDocument;
 
     registration = {
@@ -80,28 +99,36 @@ export async function POST(
       status: getRegistrationStatus(buyerCheckoutData),
       userId: authenticatedUser.uid,
       ...body.registration,
-    }
+    };
 
-    const registrationDocRef = firestore.collection("registrations").doc(checkoutDocId);
+    const registrationDocRef = firestore
+      .collection("registrations")
+      .doc(checkoutDocId);
 
     await registrationDocRef.set(registration);
   } catch (error) {
     return createErrorResponse("Erro ao criar inscrição", 500);
   }
 
-  return createSuccessResponse<CreateVoucherCheckoutResponse>({
-    checkoutId: checkoutDocId,
-    checkout,
-    registrationId: checkoutDocId,
-    registration,
-  }, 200);
+  return createSuccessResponse<CreateVoucherCheckoutResponse>(
+    {
+      checkoutId: checkoutDocId,
+      checkout,
+      registrationId: checkoutDocId,
+      registration,
+    },
+    200
+  );
 }
 
-function getRegistrationStatus(buyerCheckoutData: CheckoutDocument): RegistrationStatus {
+function getRegistrationStatus(
+  buyerCheckoutData: CheckoutDocument,
+): RegistrationStatus {
+  const isCommitmentCheckout = isPaymentByCommitment(buyerCheckoutData);
   if (buyerCheckoutData.status === "completed") {
     return "ok";
   } else if (buyerCheckoutData.status === "pending") {
-    return "pending";
+    return isCommitmentCheckout ? "ok" : "pending";
   } else {
     return "invalid";
   }
